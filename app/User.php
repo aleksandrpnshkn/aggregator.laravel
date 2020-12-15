b21d4e9520c347ebeb1de4113c5b9d8b536d82c6
aleksandr <aleksandrkrzhn@yandex.ru>
2020-12-15 20:52:23 +0400

    Включить верификацию почты

--- app/User.php
+++ app/User.php
@@ -3,12 +3,14 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
+    use MustVerifyEmailTrait;
 
     /**
      * The attributes that are mass assignable.
